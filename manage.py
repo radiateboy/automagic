1d7ef3818cb4ff9ddbdb9e4d9ccef94d66366629
ray <tsbc@qq.com>
2020-05-09 14:41:04 +0800

    去掉无用的引入库

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import MySQLdb
+
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'automatic.settings.common')
