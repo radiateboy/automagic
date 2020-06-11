29a8cc77a15d5ac8576dbb7f97cb15b74eb43778
ray <tsbc@qq.com>
2020-06-11 19:00:02 +0800

    向后台注册产品管理模块

--- automatic/management/admin.py
+++ automatic/management/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from automatic.management import models
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (id, 'name', 'isenabled', 'descr','createat', 'createtime', 'updateat', 'updatetime')
+    search_fields = ('name','descr')
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (id, 'name', 'isenabled','version','descr','createat', 'createtime', 'updateat', 'updatetime')
+    search_fields = ('name','descr','version')
+
+
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = (id, 'name', 'isenabled', 'createat', 'createtime', 'updateat', 'updatetime')
+    search_fields = ('name',)
+
+
+admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.Project, ProjectAdmin)
+admin.site.register(models.Module, ModuleAdmin)
