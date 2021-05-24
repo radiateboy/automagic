00de420279aa1a7735df3425da9a4f8044b355d3
ray <tsbc@vip.qq.com>
2021-05-24 18:30:23 +0800

    变更数据库密码

--- init_database.py
+++ init_database.py
@@ -1,6 +1,6 @@
 import MySQLdb
 
-conn = MySQLdb.Connect(host='127.0.0.1', port=int(3306), user='root', passwd='123456')
+conn = MySQLdb.Connect(host='localhost', port=int(3306), user='root', passwd='root')
 db = 'automatic'
 cur = conn.cursor()
 if not cur.execute("SELECT * FROM information_schema.SCHEMATA where SCHEMA_NAME='" + db + "'"):
