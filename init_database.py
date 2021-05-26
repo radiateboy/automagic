e2c9488c92d5d52eda79fbe6c9782dab2c9f3de5
ray <tsbc@qq.com>
2021-05-26 18:49:55 +0800

    删除旧文件
    Signed-off-by: ray <tsbc@vip.qq.com>

--- init_database.py
+++
@@ -1,9 +0,0 @@
-import MySQLdb
-
-conn = MySQLdb.Connect(host='localhost', port=int(3306), user='root', passwd='root')
-db = 'automatic'
-cur = conn.cursor()
-if not cur.execute("SELECT * FROM information_schema.SCHEMATA where SCHEMA_NAME='" + db + "'"):
-    sql_create_db = "create database " + db + " DEFAULT CHARSET utf8 COLLATE utf8_general_ci"
-    cur.execute(sql_create_db)
-cur.close()
